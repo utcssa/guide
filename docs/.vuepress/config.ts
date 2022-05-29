b69679d6482d05e28db6b3e0d8c9f467668382b7
Alex Kuang <17879507+alexkuang0@users.noreply.github.com>
2022-05-29 17:16:40 -0500

    More translations

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,5 @@
 import { defineUserConfig, defaultTheme } from 'vuepress'
-import { navbar } from './configs/navbar'
-import { sidebar } from './configs/sidebar'
+import { navbar, sidebar } from './configs'
 import { mdEnhancePlugin } from 'vuepress-plugin-md-enhance'
 
 export default defineUserConfig({
@@ -16,6 +15,22 @@ export default defineUserConfig({
     tip: '💡 小贴士',
     warning: '⚠️ 注意',
     danger: '🚨 警告',
+    // page meta
+    editLinkText: '编辑此页',
+    lastUpdatedText: '上次更新于',
+    contributorsText: '贡献者',
+    // 404 page
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接',
+    ],
+    backToHome: '返回首页',
+    // a11y
+    openInNewWindow: '在新窗口打开',
+    toggleDarkMode: '切换夜间模式',
+    toggleSidebar: '切换侧边栏',
   }),
   markdown: {
     linkify: true,
